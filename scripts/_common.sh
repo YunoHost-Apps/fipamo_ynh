6ed0e0fa2273f0b20dae573b96fccf4fec9fc8c9
ericgaspar <junk.eg@free.fr>
2022-05-19 16:28:15 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION="8.0"
 # Composer version
 YNH_COMPOSER_VERSION="2.3.3"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli"
+pkg_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
 
 #=================================================
 # PERSONAL HELPERS
